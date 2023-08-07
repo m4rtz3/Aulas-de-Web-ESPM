f013908fa16055edab9defdc8a7e22876853bb08
martz3 <menezesmarty@gmail.com>
2023-08-07 10:34:09 -0300

    arrumando

---
+++ scripts/script01.js
@@ -0,0 +1,18 @@
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", analise);
+
+function analise() {
+    let numero = parseInt(document.getElementById("numeroAnalise").value);
+    let analiseConcl = document.getElementById("analiseConc");
+
+    if (numero === 1) {
+        numero = "STOP";
+    } else if (numero % 2 === 1) {
+        numero = (3 * numero) + 1;
+    } else {
+        numero = numero / 2;
+    }
+
+    analiseConcl.innerText = numero;
+}
